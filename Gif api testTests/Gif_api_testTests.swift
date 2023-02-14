f8ec69e497233b510df0a10d453bec7f155d1a9b
Suraj Jaiswal <suraj.jaiswal@docquity.com>
2023-02-14 09:58:01 +0530

    Initial Commit

---
+++ Gif api testTests/Gif_api_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gif_api_testTests.swift
+//  Gif api testTests
+//
+//  Created by Suraj Jaiswal on 14/02/23.
+//
+
+import XCTest
+@testable import Gif_api_test
+
+final class Gif_api_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
